d9161c3e2dc4ee36b631faf0c4c36a354e9b4665
Kisaragi Marine <kisaragi.effective@gmail.com>
2023-11-02 13:46:27 +0900

    feat(example): take a path to a file

--- examples/frdt_ex.rs
+++ examples/frdt_ex.rs
@@ -1,8 +1,22 @@
-use std::io::BufReader;
+use std::process::ExitCode;
+use frooxengine_data_tree::DeserializeError;
 
-fn main() {
-    let file = std::fs::read("/home/you/.config/Example/example.brson").expect("fail");
+fn main() -> ExitCode {
+    match main_inner() {
+        Ok(()) => ExitCode::SUCCESS,
+        Err(e) => {
+            eprintln!("{e}");
+            ExitCode::FAILURE
+        }
+    }
+}
+
+fn main_inner() -> Result<(), DeserializeError> {
+    let file = std::env::args().nth(1).expect("Usage: [path-to-file]");
+    let file = std::fs::read(file)?;
     let m = frooxengine_data_tree::split_froox_container_header(&file).unwrap_or_else(|_| frooxengine_data_tree::legacy(&file));
-    let b = m.deserialize::<bson::Bson>().expect("fail");
+    let b = m.deserialize::<bson::Bson>()?;
     println!("{b:?}");
+
+    Ok(())
 }
